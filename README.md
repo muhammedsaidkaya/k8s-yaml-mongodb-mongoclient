5eb7d91cd4cc167c42d6b707238387ec898998cb
muhammedsaidkaya <b2167428@cs.hacettepe.edu.tr>
2021-08-19 18:18:11 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# local-mongodb-setup
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#add path
+declare -a StringArray=( \
+"mongodb-secret.yaml" \
+"mongodb-configmap.yaml" \
+"mongodb-deployment.yaml" \
+"mongodb-service.yaml" \
+"mongoclient-deployment.yaml" \
+"mongoclient-service.yaml" \
+)
+
+for value in ${StringArray[@]};
+do
+    kubectl apply -f $value
+done
